de5f65dd4d435ac10c576b502b3f3ab0befdb80a
Vishal Gupta <guptaaryan1103@gmail.com>
2026-04-29 15:56:23 +0530

    Create APIs for book store

--- 07_Introduction_to_Postman/index.js
+++ 07_Introduction_to_Postman/index.js
@@ -2,17 +2,31 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+// Book Store
+
+const bookStore = [
+  {id: 1, name: "English"},
+]
+
 // Parsing
 app.use(express.json());
 
-app.get("/user", (req, res) => {
-  res.send("Vishal Gupta");
-});
+app.get("/book", (req, res) => {
+  res.send(bookStore);
+})
 
-app.post("/user", (req, res) => {
-  console.log(req.body)
-  res.send({ data: "Data saved successfully!", success: true });
-});
+app.get("/book/:id", (req, res) => {
+
+  const id = parseInt(req.params.id);
+  const book = bookStore.find(info => info.id === id);
+
+  res.send({data: book, success: true});
+})
+
+app.post("/addbook", (req, res) => {
+  bookStore.push(req.body);
+  res.send({data: bookStore, success: true});
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}`);

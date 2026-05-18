4b26c2fabd66d3ead8e5b017f392f7819f496389
Vishal Gupta <guptaaryan1103@gmail.com>
2026-05-18 22:58:03 +0530

    register api created

--- 15_Dive into Mongoose/index.js
+++ 15_Dive into Mongoose/index.js
@@ -1,15 +1,52 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./database");
-dotenv.config({})
+const User = require("./user.model");
+dotenv.config({});
 const app = express();
 const PORT = 4000;
 
+app.use(express.json());
+
 app.use("/", (req, res) => {
-    res.send("Testing Route")
-})
+  res.send("Testing Route");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.params;
+
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Something is missing here!!", status: false });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res
+        .status(400)
+        .json({ message: "User is already present!", status: false });
+    }
+
+    const newUser = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    return res
+      .status(201)
+      .json({ message: "User Created Successfully!", data: newUser });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Something went wrong here!", success: false });
+  }
+});
 
 app.listen(PORT, () => {
-    connectDB();
-    console.log(`The server is running at PORT ${PORT}`);
-})
+  connectDB();
+  console.log(`The server is running at PORT ${PORT}`);
+});
---
+++ 15_Dive into Mongoose/user.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+})
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;

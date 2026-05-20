f80acd29b958242353af86dfcae3839389ac86d5
Vishal Gupta <guptaaryan1103@gmail.com>
2026-05-20 16:45:17 +0530

    Create Login API

--- 17_Data Senitization and Validation/src/controllers/user.controller.js
+++ 17_Data Senitization and Validation/src/controllers/user.controller.js
@@ -3,6 +3,7 @@ const router = express.Router;
 const bcrypt = require("bcrypt");
 const User = require("../models/user.model");
 
+// Register
 const register = async (req, res) => {
   try {
     const mandatoryField = [
@@ -40,6 +41,7 @@ const register = async (req, res) => {
         .json({ message: "User is already present!", status: false });
     }
 
+    // HashCode + Salt
     const hashPassword = await bcrypt.hash(password, 10);
 
     const response = await User.create({
@@ -66,4 +68,45 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+// Login
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Something is missing!", status: false });
+    }
+
+    const user = await User.findOne({ email });
+    console.log(user);
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "User is not present in database!", status: false });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    console.log(isPasswordMatch);
+
+    if (!isPasswordMatch) {
+      return res
+        .status(400)
+        .json({ message: "Invalid Credientials!", status: false });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "User login successfully!", data: user, status: true });
+
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ message: err.message || "Internal Server Error", status: false });
+  }
+};
+
+module.exports = { register, login };

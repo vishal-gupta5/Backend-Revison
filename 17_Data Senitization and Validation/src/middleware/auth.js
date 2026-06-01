9942f2d0ecbb11d3623e724ffb7501a961eecb2f
hyannu <annurao9560@gmail.com>
2026-06-01 22:42:16 +0530

    Create Middleware for authentication

---
+++ 17_Data Senitization and Validation/src/middleware/auth.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model")
+
+const auth = async (req, res, next) => {
+  try {
+    // Find out the token from cookies
+    const { token } = req.cookies;
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Token is not present!", status: false });
+    }
+
+    // Verify the token
+    const decode = await jwt.verify(token, process.env.JWT_KEY);
+
+    const user = await User.findOne({ email: decode.email });
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "User not found!", status: false });
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({
+      message: "Something went wrong!",
+      status: false,
+    });
+  }
+};
+
+module.exports = auth;
--- 17_Data Senitization and Validation/src/routes/user.auth.route.js
+++ 17_Data Senitization and Validation/src/routes/user.auth.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { register, login } = require("../controllers/user.controller");
+const auth = require("../middleware/auth");
 const userAuth = express.Router();
 
-userAuth.post("/register", register);
-userAuth.post("/login", login);
+userAuth.post("/register", auth, register);
+userAuth.post("/login", auth, login);
 
 module.exports = userAuth;
